2114c3fac76828285bfd6433269b4d16537d182b
drumath <2237k.drumath@gmail.com>
2020-06-12 11:49:37 +0900

    :sparkles: テキトーなページが動作した

---
+++ pages/facemesh-test.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Hello, {{name}}
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      name: "ninisan"
+    }
+  }
+})
+</script>
