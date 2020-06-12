1a70601849c93178570717dae5c85afdaa23ef8d
drumath <2237k.drumath@gmail.com>
2020-06-13 03:25:19 +0900

    :sparkles: canvasを内包したコンポーネントのエラーを除いた

--- components/ThreeCanvas.vue
+++ components/ThreeCanvas.vue
@@ -3,8 +3,22 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
+import TestCanvas from "./src/TestCanvas";
+
+type DataType = {
+  artwork: TestCanvas | null;
+};
+
 export default Vue.extend({
-  
-})
+  data(): DataType {
+    return {
+      artwork: null
+    };
+  },
+  mounted() {
+    this.artwork = new TestCanvas(<HTMLCanvasElement>this.$refs.canvas);
+    this.artwork.loop();
+  }
+});
 </script>
