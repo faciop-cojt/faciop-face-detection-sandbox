b30a29e6417d2121c80b94ffb0d7084364f6218a
drumath <2237k.drumath@gmail.com>
2020-06-24 03:11:02 +0900

    :sparkles: video用のスクリプト、いったんここまで

---
+++ plugins/FacemeshVideoObject.ts
@@ -0,0 +1,29 @@
+export class FacemeshVideo {
+  size: { w: number; h: number };
+  resolution: { w: number; h: number };
+  autoplay: boolean;
+
+  constructor() {
+    this.size = { w: 360, h: 240 };
+    this.resolution = { w: 1920, h: 1080 };
+    this.autoplay = true;
+  }
+
+  setSize(size: { w: number; h: number }) {
+    this.size = size;
+  }
+
+  setResolution(resolution: { w: number; h: number }) {
+    this.resolution = resolution;
+  }
+
+  getWebCamStream(): Promise<MediaStream> {
+    return navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        width: { ideal: this.resolution.w },
+        height: { ideal: this.resolution.h }
+      }
+    });
+  }
+}
