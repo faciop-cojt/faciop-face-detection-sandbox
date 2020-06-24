32996190f984e3432bc7688aee9400a4dd402ce0
drumath <2237k.drumath@gmail.com>
2020-06-24 15:15:16 +0900

    :bug: バックエンドの指定をしたつもりだが、やっぱり動かない...

--- plugins/FacemeshProvider.ts
+++ plugins/FacemeshProvider.ts
@@ -1,24 +1,29 @@
 import * as facemesh from "@tensorflow-models/facemesh";
 import * as tf from "@tensorflow/tfjs-core"
-// import * as tfjsWasm from "@tensorflow/"
-// import Stats from "stats.js"
+import * as tfjsWasm from "@tensorflow/tfjs-backend-wasm"
+import Stats from "stats.js"
 
 import Vue from "vue";
 
 export class FacemeshProvider {
   model: Promise<facemesh.FaceMesh>;
+  stats: Stats
 
   constructor() {
     this.model = facemesh.load();
+    this.stats = new Stats();
+    tf.setBackend('wasm');
   }
 
   getFacemeshPoints(video: HTMLVideoElement) {
+    this.stats.begin();
     this.model.then(model => {
       model.estimateFaces(video)
       .then(prediction=>{
         console.log(prediction);
         
       })
+      this.stats.end();
     })
   }
 }
