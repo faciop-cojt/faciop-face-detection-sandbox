1ca6882fd0cbddd500f14b0648b9abdf44eff819
drumath <2237k.drumath@gmail.com>
2020-07-09 04:33:32 +0900

    :sparkles: メッシュの生成やシーンの構築などを加筆

--- plugins/FaceCanvas.ts
+++ plugins/FaceCanvas.ts
@@ -3,27 +3,72 @@ import * as THREE from "three";
 import * as Renderable from "./IRenderable";
 import { IFaceDataSettable } from "./IFaceDataSettable";
 
+import { FaceMeshFaceGeometry } from "./FacemeshFaceGeometry/face";
+
+import * as facemesh from "@tensorflow-models/facemesh";
+
 export class FaceCanvas implements Renderable.IRenderable, IFaceDataSettable {
   private canvas?: HTMLElement;
   private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
 
-  private camera?: THREE.OrthographicCamera;
+  private camera: THREE.OrthographicCamera;
 
-  constructor(canvas?: Renderable.CanvasParameters | undefined) {
+  private face_obj?: THREE.Mesh;
+  private face_geometry: FaceMeshFaceGeometry;
+
+  constructor(canvas?: Renderable.CanvasParameters) {
+    // initialize
     this.canvas = canvas?.canvas || undefined;
-    this.renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer({
+      canvas: canvas?.canvas
+    });
     this.scene = new THREE.Scene();
 
-    if(canvas?.width && canvas?.height){
+    this.camera = new THREE.OrthographicCamera(-1,1,1,-1);
+    if (canvas?.width && canvas?.height) {
       this.camera = new THREE.OrthographicCamera(
-        -canvas.width/2.0, canvas.width/2.0, canvas.height/2.0, -canvas.height/2.0
+        -canvas.width / 2.0,
+        canvas.width / 2.0,
+        canvas.height / 2.0,
+        -canvas.height / 2.0
       );
+      this.camera.updateProjectionMatrix();
     }
+    this.face_geometry = new FaceMeshFaceGeometry({
+      normalizeCoords: false,
+      useVideoTexture: false
+    });
+    let face_mat = new THREE.MeshStandardMaterial({
+      color: "#666"
+    });
+    this.face_obj = new THREE.Mesh(this.face_geometry, face_mat);
+
+    // TODO: scene creation
+
+    this.scene.add(this.face_obj);
+
+    let light = new THREE.DirectionalLight('#fff');
+    this.scene.add(light);
 
   }
   setCanvas(canvas: Renderable.CanvasParameters): void {
-    throw new Error("Method not implemented.");
+    this.canvas = canvas.canvas || this.canvas;
+    if(canvas.canvas){
+      this.renderer = new THREE.WebGLRenderer({
+        canvas: canvas.canvas
+      })
+    }
+
+    if (canvas?.width && canvas?.height) {
+      this.camera = new THREE.OrthographicCamera(
+        -canvas.width / 2.0,
+        canvas.width / 2.0,
+        canvas.height / 2.0,
+        -canvas.height / 2.0
+      );
+      this.camera.updateProjectionMatrix();
+    }
   }
   render(): void {
     throw new Error("Method not implemented.");
@@ -31,9 +76,7 @@ export class FaceCanvas implements Renderable.IRenderable, IFaceDataSettable {
   loop(): void {
     throw new Error("Method not implemented.");
   }
-  setFaceData(
-    face: import("@tensorflow-models/facemesh").AnnotatedPrediction
-  ): void {
+  setFaceData(face: facemesh.AnnotatedPrediction): void {
     throw new Error("Method not implemented.");
   }
 }
