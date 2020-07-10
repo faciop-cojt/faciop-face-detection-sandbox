51cb72b1c27485b0ad8167bf277ece2578265459
drumath <2237k.drumath@gmail.com>
2020-07-10 17:55:56 +0900

    :sparkles: videoがreadyになったときのイベントをちょっと書いた

--- components/FacemeshVideo.vue
+++ components/FacemeshVideo.vue
@@ -7,14 +7,12 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 export default Vue.extend({
-  data() {
-    return {
-      msg: "hello"
-    }
+  mounted() {
+    (<HTMLVideoElement>this.$refs.video).onloadeddata = ev => {};
   }
-})
+});
 </script>
 
 <style lang="scss" scoped></style>
