5dfb22fe88fa5e77a8830c7bd3bf8b4a4c34274f
drumath <2237k.drumath@gmail.com>
2020-07-10 17:44:17 +0900

    :fire: とりあえずconponentの中身を消してバグの出ない範囲でいろいろ消した

--- components/FacemeshVideo.vue
+++ components/FacemeshVideo.vue
@@ -7,35 +7,14 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { FacemeshVideo } from "~/plugins/FacemeshVideoObject";
-import { FacemeshProvider } from "~/plugins/FacemeshProvider";
-import { log } from "@tensorflow/tfjs-core";
-
-import {AnnotatedPrediction} from "@tensorflow-models/facemesh"
-
+import Vue from 'vue'
 export default Vue.extend({
   data() {
     return {
-      facemesh_video: new FacemeshVideo()
-    };
-  },
-  mounted() {
-    this.facemesh_video.initVideoObject(<HTMLVideoElement>this.$refs.video);
-    (<HTMLVideoElement>this.$refs.video).addEventListener('loadeddata', (event)=>{
-      this.loop();
-    })
-  },
-  methods: {
-    loop(){
-      this.$facemeshProvider.getFacemeshPointsAsync(<HTMLVideoElement>this.$refs.video)
-      .then((predictions:AnnotatedPrediction[]) =>{
-        this.$nuxt.$emit('updateFacemesh', predictions[0].scaledMesh)
-      })
-      requestAnimationFrame(this.loop)
+      msg: "hello"
     }
   }
-});
+})
 </script>
 
 <style lang="scss" scoped></style>
